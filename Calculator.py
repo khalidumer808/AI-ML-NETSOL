1b52e68d41f66275634a38efcf22e3efc7735f26
khalidumer808 <khalidumer808@gmail.com>
2026-04-20 12:05:56 +0500

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,15 @@
-print('hello world')
+class Calculator:
+
+  def add(a, b):
+      return a + b
+
+  def subtract(a, b):
+      return a - b
+
+  def multiply(a, b):
+      return a * b
+
+  def divide(a, b):
+      if b == 0:
+          return "Error: Cannot divide by zero"
+      return a / b
